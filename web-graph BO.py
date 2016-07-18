951e28a9a9b885ed250a6aa641cffba514b9e0fa
Kvonsiun <Daniil.smirnov.2000@inbox.ru>
2016-07-18 22:38:31 +0400

    Update web-graph BO.py

--- web-graph BO.py
+++ web-graph BO.py
@@ -1,36 +1,63 @@
 from random import randint as rnd
+import time
+
 matrix = open('probability graph.txt', 'w')
+#data = open("inbox_data", "w")
 
-V = 1000
+V = 50000
+K = 10
+a = 10000
 
-def rand(i):
-    if rnd(1, (i + 1) * 2 - 1) == 1:
-        return(i)
+def rand(n):
+    f = rnd(1, (a + 1) * n)
+    if f == 1:
+        return(n)
+    elif f <= a:
+        return(rnd(0, n - 1))
     else:
-        return(vertexs[rnd(0, i * 2 - 2)])
+        return(vertexs[rnd(0, len(vertexs) - 1)])
 
 def edge(j, i):
-    graph[i][j] = 1
-    graph[j][i] = 1
     vertexs.append(j)
     vertexs.append(i)
+    graph[i // K][j // K] += 1
+    #inbox[i // K].append(j // K)
+    if j // K != i // K:
+        graph[j // K][i // K] += 1
+        #inbox[j // K].append(i // K)
+    numbers[i // K] += 1
+    numbers[j // K] += 1
 
-def for_print(graph):
-    matrix.write(str(V))
+
+def for_print():
+    matrix.write(str(V // K))
     matrix.write("\n")
-
-    for i in range(V):
+    #data.write(str(V // K))
+    #data.write("\n")
+    for i in range(V // K):
         matrix.write(" ".join(map(str, graph[i])))
         matrix.write("\n")
+        #data.write(" ".join(map(str, inbox[i])))
+        #data.write("\n")
+
+def division():
+    for i in range(V // K):
+        for j in range(V // K):
+            graph[i][j] /= numbers[i]
 
 def main():
-    global graph, vertexs
-    graph = [[0] * V for i in range(V)]
+    global vertexs, graph, numbers, inbox
+    starttime = time.time()
+    graph = [[0] * (V // K) for i in range(V // K)]
+    numbers = [0] * (V // K)
     graph[0][0] = 1
     vertexs = [0]
+    #inbox = [[] for i in range(V // K)]
     for i in range(1, V):
         j = rand(i)
         edge(j, i)
-    for_print(graph)
+    division()
+    for_print()
+    print(time.time() - starttime)
 
 main()
