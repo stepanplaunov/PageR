55a7eca4e5f721dbd2176e7b344511fa63697656
KoIIdun <styopa.plaunov@gmail.com>
2016-11-18 23:41:31 +0400

    Update fgm.py

--- prlib_for_100k/fgm.py
+++ prlib_for_100k/fgm.py
@@ -7,16 +7,13 @@ def read(graph):
     for i in range(n):
         graph[i, i] -= 1
     graph = graph.transpose()
-    graph.resize((n + 1, n))
-    for i in range(n):
-        graph[-1, i] = 1.0
     return graph.tocoo().tocsr(), n
 
 def norma(vector):
     return np.linalg.norm(vector, 2)
 
-def f(A, x, b):
-    return 0.5 * norma(A.dot(x) - b) ** 2
+def f(A, x):
+    return 0.5 * np.linalg.norm(A.dot(x)) ** 2
 
 def grad(A, x, b):
     return A.T.dot(A.dot(x) - b)
@@ -26,35 +23,11 @@ def f_(xnew, x, L, gradi, A, b):
 
 def fgm(graph):
     A, n = read(graph)
-    k = 0
-    b = np.array([0] * (n + 1))
-    b[-1] = 1.0
-    x = np.array([0] * n)
+    x = np.array([0.0] * n)
     x[0] = 1.0
-    EPS = 10 ** (-3)
-    L = 10
-    y = x.copy()
-    z = x.copy()
-    ynew = (2 * z + k * x) / (k + 2)
-    grady = grad(A, ynew, b)
-    xnew = ynew - grady / L
-    znew = z - (k + 2) * grady / (2 * L)
-    k += 1
-    while f(A, x, b) > EPS:
-        while f_(xnew, ynew, L, grady, A, b) < f(A, xnew, b):
-            L *= 2
-            grady = grad(A, ynew, b)
-            xnew = ynew - grady / L
-            znew = z - (k + 2) * grady / (2 * L)
-        x, y, z = xnew, ynew, znew
-        grady = grad(A, ynew, b)
-        L /= 2
-        k += 1
-        ynew = (2 * z + k * x) / (k + 2)
-        xnew = ynew - grady / L
-        znew = z - (k + 2) * grady / (2 * L)
-    print(x.sum())
-    x += abs(min(x)) + 0.000000001
-    print(x)
+    EPS = 10 ** (-7)
+    while f(A, x) > EPS:
+        x = A.dot(x) + x
     x = x / x.sum()
+    print(*x)
     return x
